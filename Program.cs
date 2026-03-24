451148fb8a91dde27f93bd9bfc852664a79a85fb
KarloERC <karlo.ercegovic10@gmail.com>
2026-03-24 19:36:21 +0100

    nova2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Novo");
 Console.WriteLine("Novo!!!");
 Console.WriteLine("Tu eres KaRLO!!!");
 
+internal class NewClass
+{
+
+}
 //dodao sam jos jedan writeLine
